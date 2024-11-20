0ba6591858c5d1ed7f71cbebb3a24a6757ce04b1
Peter <phuchoang.inf@gmail.com>
2024-11-20 23:53:43 +0700

    feat: add answer 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {AppComponent} from "./app.component";
-import {HelloComponent} from "./hello.component";
-import {Test1Component} from "./test1/test1.component";
-import {Test2Component} from "./test2/test2.component";
-import {AppRoutingModule} from "./app-routing.module";
-import {HomeComponent} from "./home/home.component";
-import {Test3Component} from "./test3/test3.component";
-import {HeavyComponent} from "./test3/heavy/heavy.component";
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { Test3Component } from './test3/test3.component';
+import { HeavyComponent } from './test3/heavy/heavy.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
@@ -20,9 +20,8 @@ import {HeavyComponent} from "./test3/heavy/heavy.component";
     Test1Component,
     Test2Component,
     Test3Component,
-    HeavyComponent
+    HeavyComponent,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,22 +1,62 @@
-import {Component, OnInit} from '@angular/core';
-import {Test1Service} from "./test1.service";
+import { Component, OnInit } from '@angular/core';
+import { Test1Service } from './test1.service';
+import {
+  FormBuilder,
+  FormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.scss'],
-  providers: [Test1Service]
+  providers: [Test1Service],
 })
 export class Test1Component implements OnInit {
+  public form!: UntypedFormGroup;
+  protected destroy$ = new Subject<void>();
 
-  constructor(private test1Service: Test1Service) {
-  }
+  constructor(
+    private test1Service: Test1Service,
+    private _formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.setupForms();
+
+    this.form
+      .get('type')!
+      .valueChanges.pipe(takeUntil(this.destroy$), distinctUntilChanged())
+      .subscribe({
+        next: (val) => {
+          if (val === 'car') {
+            this.form.removeControl('color');
+            this.form.addControl(
+              'brand',
+              new FormControl(null, [Validators.required])
+            );
+          } else {
+            this.form.removeControl('brand');
+            this.form.addControl(
+              'color',
+              new FormControl(null, [Validators.required])
+            );
+          }
+        },
+      });
   }
 
-  submit(value: any) {
+  public submit(value: any) {
     this.test1Service.test(value);
   }
 
+  private setupForms(): void {
+    this.form = this._formBuilder.group({
+      name: new FormControl(null, [Validators.required]),
+      type: new FormControl('', [Validators.required]),
+    });
+  }
 }
