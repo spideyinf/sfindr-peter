82c39e33ac0747c28acd8caf8b39b8d745401a99
Peter <phuchoang.inf@gmail.com>
2024-11-21 09:50:17 +0700

    feat: add answer test 3

--- src/app/test3/heavy/heavy.component.ts
+++ src/app/test3/heavy/heavy.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Row} from '../row.interface';
+import { Component, Input, OnInit } from '@angular/core';
+import { Row } from '../row.interface';
 
 // don't make any changes to this component!
 @Component({
   selector: 'app-heavy',
   template:
-    '<h2>{{ row.title }}</h2><p>Last fetched: {{ row.lastFetched|date:"medium" }}</p><p>Total: {{ total }}</p>'
+    '<h2>{{ row.title }}</h2><p>Last fetched: {{ row.lastFetched|date:"medium" }}</p><p>Total: {{ total }}</p>',
 })
 export class HeavyComponent implements OnInit {
   @Input() row!: Row;
--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -1,24 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {delay, repeatWhen, tap} from 'rxjs/operators';
-import {Row} from './row.interface';
-import {source} from "./source";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { delay, repeatWhen } from 'rxjs/operators';
+import { Row } from './row.interface';
+import { source } from './source';
 
 @Component({
   selector: 'app-test3',
   templateUrl: './test3.component.html',
-  styleUrls: ['./test3.component.scss']
+  styleUrls: ['./test3.component.scss'],
 })
 export class Test3Component implements OnInit {
   rows$?: Observable<Row[]>;
+  private initialized = false;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.rows$ = source
-      .pipe(
-        repeatWhen(x => x.pipe(delay(5000))),
-      );
+    if (!this.initialized) {
+      this.rows$ = source.pipe(repeatWhen((x) => x.pipe(delay(5000))));
+      this.initialized = true;
+    }
   }
 }
